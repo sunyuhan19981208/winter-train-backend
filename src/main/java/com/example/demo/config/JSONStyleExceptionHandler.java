ff588e0f676784548b642745cec34971b33b1820
chengza <1727180069@qq.com>
2020-12-16 16:10:23 +0800

    add an exception handler to show reason of bad requests

---
+++ src/main/java/com/example/demo/config/JSONStyleExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class JSONStyleExceptionHandler {
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseBody
+    public Map handleAnyException(HttpServletRequest request, Exception e) {
+        return new HashMap<String, Object>() {
+            {
+                put("request path", request.getServletPath());
+                put("exception message",
+                        e instanceof NoSuchElementException ? "Cannot find destination object in database for this request" : e.getMessage());
+
+            }
+        };
+    }
+}
