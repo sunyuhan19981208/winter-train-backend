4bbb60204ac2770f24fb0b1ae93b1504bea5570d
chengza <1727180069@qq.com>
2020-12-17 14:54:52 +0800

    fix register and login error; store error log.

--- src/main/java/com/example/demo/config/JSONStyleExceptionHandler.java
+++ src/main/java/com/example/demo/config/JSONStyleExceptionHandler.java
@@ -6,16 +6,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
-import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class JSONStyleExceptionHandler {
+    private static PrintStream errorLog = null;
 
     @ExceptionHandler(Throwable.class)
     @ResponseBody
     public HashMap<String, Object> handleAnyException(HttpServletRequest request, HttpServletResponse response,
                                                       Exception e) {
+        e.printStackTrace();
+        try {
+            if (errorLog == null) {
+                errorLog = new PrintStream(new FileOutputStream(new File("error.log"), true));
+            }
+            e.printStackTrace(errorLog);
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
+
         response.setStatus(500);
         return new HashMap<String, Object>() {
             {
